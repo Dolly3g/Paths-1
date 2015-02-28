087d04e87d36eb07a0624371104ad690016aac66
GautamSidtharth <sidtharn@thoughtworks.com>
2015-02-28 17:35:10 +0530

    setRoute and checkRoute are implemented for firstTime storing and accessing

--- Paths.java
+++ Paths.java
@@ -1,2 +1,20 @@
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 class Paths {
+	public Map<String,List<String>> flightsMap;
+	public Paths(){
+		flightsMap=new HashMap<String,List<String>>();
+	}
+	public void setRoute(String cityFrom, String cityTo){
+		List<String> allDestinations=new ArrayList<String>();
+		allDestinations.add(cityTo);
+		flightsMap.put(cityFrom,allDestinations);
+	}
+	public String checkRoute(String cityFrom, String cityTo){
+		boolean availability=flightsMap.containsKey(cityFrom);
+		return availability? "true" : "false";
+	}
 }
--- PathsTest.java
+++ PathsTest.java
@@ -2,8 +2,21 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PathsTest {
+	public Paths flightPaths=new Paths();
+	public void setUp(){
+		flightPaths=new Paths();
+	}
 	@Test
-	public void runs(){
-		assertEquals(1,1);
+	public void checkRoute_returns_true_for_available_route(){
+		setUp();
+		flightPaths.setRoute("Bangalore","Singapore");
+		String availability=flightPaths.checkRoute("Bangalore","Singapore");
+		assertEquals("true",availability);
+	}
+	@Test
+	public void checkRoute_returns_false_when_no_flights_available_from_given_city(){
+		setUp();
+		String availability=flightPaths.checkRoute("Bangalore","Singapore");
+		assertEquals("false",availability);
 	}
 }
