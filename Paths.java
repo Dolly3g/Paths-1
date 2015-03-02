94cf67efd8a0b171abb7d9a7760d8a00276b22eb
GautamSidtharth <sidtharn@thoughtworks.com>
2015-03-02 10:11:42 +0530

    Added main in paths | name changed setRoute > addPath | name changed checkRoute > hasPath

--- Paths.java
+++ Paths.java
@@ -8,13 +8,23 @@ class Paths {
 	public Paths(){
 		flightsMap=new HashMap<String,List<String>>();
 	}
-	public void setRoute(String cityFrom, String cityTo){
+	public void addPath(String cityFrom, String cityTo){
 		List<String> allDestinations=new ArrayList<String>();
 		allDestinations.add(cityTo);
 		flightsMap.put(cityFrom,allDestinations);
 	}
-	public String checkRoute(String cityFrom, String cityTo){
+	public String hasPath(String cityFrom, String cityTo){
 		boolean availability=flightsMap.containsKey(cityFrom);
 		return availability? "true" : "false";
+	}
+	public static void main(String[] args) {
+		Paths paths=new Paths();
+		paths.addPath("Bangalore","Singapore");
+		paths.addPath("Singapore","Seoul");
+		paths.addPath("Singapore","Dubai");
+		paths.addPath("Seoul","Beijing");
+		paths.addPath("Beijing","Tokyo");
+		String availability=paths.hasPath(args[0],args[1]);
+		System.out.println(availability);
 	}
 }
--- PathsTest.java
+++ PathsTest.java
@@ -9,14 +9,14 @@ public class PathsTest {
 	@Test
 	public void checkRoute_returns_true_for_available_route(){
 		setUp();
-		flightPaths.setRoute("Bangalore","Singapore");
-		String availability=flightPaths.checkRoute("Bangalore","Singapore");
+		flightPaths.addPath("Bangalore","Singapore");
+		String availability=flightPaths.hasPath("Bangalore","Singapore");
 		assertEquals("true",availability);
 	}
 	@Test
 	public void checkRoute_returns_false_when_no_flights_available_from_given_city(){
 		setUp();
-		String availability=flightPaths.checkRoute("Bangalore","Singapore");
+		String availability=flightPaths.hasPath("Bangalore","Singapore");
 		assertEquals("false",availability);
 	}
 }
